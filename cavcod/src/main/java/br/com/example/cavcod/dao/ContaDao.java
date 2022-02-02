d2c2d39177bb8769b3ec2684c2b800763abc1d8e
Fábio Meireles <fbomrl14@gmail.com>
2022-02-02 09:02:35 -0300

    2UP12C MINGW64 ~/Documents/CavCod (master) $ git commit -m

---
+++ cavcod/src/main/java/br/com/example/cavcod/dao/ContaDao.java
@@ -0,0 +1,15 @@
+package br.com.example.cavcod.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.example.cavcod.models.ContaMod;
+
+	public interface ContaDao extends CrudRepository<ContaMod,Integer>{
+
+
+
+	}
+	
+	
+
+
